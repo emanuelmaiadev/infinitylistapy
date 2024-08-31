d94ea90fa8f6029a4f2c05b53fba1d0f3b434043
emanuelmaiadev <emanuelmaiadev@gmail.com>
2024-08-31 19:40:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+obs = 'Tomate é uma fruta'
 frutas = ['maça', 'banana', 'morango', 'jaca', 'tomate', 'melancia']
 
 frutas_tupla = tuple(frutas)
 
 
-print(type(frutas))
+print(type(frutas_tupla))
 print(f'A tupla de frutas é {frutas_tupla}')
+print(obs)
 
